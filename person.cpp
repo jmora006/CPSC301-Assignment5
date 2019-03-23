43ad15f1a33c6c0242b0b89cb5ef795ce5e9d125
Jason Mora-Mendoza <jasonmoramendoza@gmail.com>
2019-03-22 23:25:14 -0700

    Added necessary code into member functions

--- person.cpp
+++ person.cpp
@@ -1,28 +1,39 @@
 #include "person.h"
 
-Person::Person(int cardNo, bool act, string fName, string lName) {
-    // complete constructor
+Person::Person(int cardNo, bool act, string fName, string lName)
+{
+	cardID = cardNo;
+	setActive(act);
+	firstName = fName;
+    lastName = lName;
 }
 
-string Person::getFirstName() {
-    return ""; // complete
+string Person::getFirstName()
+{
+    return firstName;
 }
 
-string Person::getLastName() {
-    return ""; // complete
+string Person::getLastName()
+{
+    return lastName;
 }
 
-int Person::getId() {
-    return 0; // complete
+int Person::getId()
+{
+    retrun cardID;
 }
 
-void Person::setActive(bool act) {
-} // complete
+void Person::setActive(bool act)
+{
+	active = act;
+}
 
-bool Person::isActive() {
+bool Person::isActive()
+{
     return true; // complete
 }
 
-string Person::fullName() {
-    return ""; // complete
+string Person::fullName()
+{
+	return firstName + " " + lastName;
 }
